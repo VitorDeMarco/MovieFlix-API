eed7ab52650b7b2243a46834f70f64f0e929116c
Vitor Hugo De Marco@VitorDeMarco <vitorhugodemarco@gmail.com>
2024-06-20 14:47:31 -0300

    adicionado ordenacao, genero e linguagem dos filmes

--- src/server.ts
+++ src/server.ts
@@ -5,11 +5,22 @@ const port = 3000;
 const app = express();
 const prisma = new PrismaClient();
 
-app.get("/movies", async (_, res) => {
-   const movies = await prisma.movie.findMany();
-   res.json(movies);
+app.get('/movies', async (_, res) => {
+    try {
+        const movies = await prisma.movie.findMany({
+            orderBy: { title: 'asc' },
+            include: {
+                genre: true,
+                language: true
+            },
+        });
+        res.json(movies);
+    } catch (error) {
+        console.error('Error fetching movies:', error);
+        res.status(500).json({ error: "An error occurred while fetching movies" });
+    }
 });
 
 app.listen(port, () => {
-   console.log(`Servidor em execução em http://localhost:${port}`);
+    console.log(`Servidor em execução em http://localhost:${port}`);
 });
